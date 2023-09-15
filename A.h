a7f97bbc0f145b8610ec3985e0e3657f1f462150
Avantika Shailesh Deshmukh <2021bit153@sggs.ac.in>
2023-09-15 10:03:03 +0530

    Add files via upload

---
+++ A.h
@@ -0,0 +1,7 @@
+#define area_circle(r)(3.13*r*r)
+#define perimeter_circle(r)(2*3.14*r)
+#define area_triangle(l,b)(0.5*l*b)
+#define perimeter_triangle(l,b,h)(l+b+h)
+#define  area_square(a)(a*a)
+#define primeter_sqaure(a)(4*a)
+
